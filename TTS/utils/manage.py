3325218c2c2d3979341b2e8dd9cfe89d3ea4412d
giaphat71 <59292551+giaphat71@users.noreply.github.com>
2025-04-27 05:13:57 +0700

    Fix loading fairseq model.

--- TTS/utils/manage.py
+++ TTS/utils/manage.py
@@ -262,6 +262,8 @@ class ModelManager(object):
         if "fairseq" in model_name:
             model_type = "tts_models"
             lang = model_name.split("/")[1]
+            dataset = "multi-dataset"
+            model = model_name
             model_item = {
                 "model_type": "tts_models",
                 "license": "CC BY-NC 4.0",
